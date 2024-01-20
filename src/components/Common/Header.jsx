05a75ae73a79c578c4d71fb27f6592813ac9b1d3
sagun03 <sagunsaluja13@gmail.com>
2024-01-20 13:22:13 -0500

    updated logo

--- src/components/Common/Header.jsx
+++ src/components/Common/Header.jsx
@@ -1,13 +1,14 @@
-import useAuth from '../../hooks/useAuth.js';
-
+import useAuth from '../../hooks/useAuth';
+import logo from '../../assets/task.png'
 const Header = () => {
   const { user } = useAuth(); // Assuming you have an authentication context or hook
 
   return (
-    <header className="fixed top-0 left-0 w-full bg-blue-500 p-4 text-white">
+    <header className="fixed top-0 left-0 w-full bg-blue-500 p-1 text-white">
       <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-white text-2xl font-bold">Task Hub</h1>
-        <nav className="flex space-x-4">
+        {/* <h1 className="text-white text-2xl font-bold"></h1> */}
+        <img src={logo} className='w-48' />
+        <nav className="text-xl font-semibold flex space-x-4">
           <ul className="flex space-x-4">
             <li className="text-white">Dashboard</li>
             <li className="text-white">Tasks</li>

ee93a6502b17f5c2dc432a8495b6d05bfa155b39
Danny MB <danielmasias4@gmail.com>
2024-02-03 22:57:06 -0500

    HomeItems.jsx and .css created and added 2 images for the homepage

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,18 @@
+import HomeItems from "../../components/HomeItems/homeItems";
+
 const Home = () => {
-  return <div>Home</div>;
-};
+  return  <>
+          <HomeItems
+            UrlImage='/ToDo.png'
+            Description='Discover the efficiency of our ToDo table on TashHhub. Organize your tasks with ease: "ToDo" for pending, 
+            "InProgress" for current and "Complete" for wins. Manage your project effectively with us! 🚀 #SimpleOrganization #ImparableProductivity.'>          </HomeItems>
+          <HomeItems
+            UrlImage='/ToDo2.png'
+            Description='Experience the power of ultimate customization with our ToDo solution! Tailor your task management experience to fit your unique style.
+             From personalized categories to flexible priority settings, take control of your productivity like never before. Embrace efficiency with
+              a ToDo platform that adapts to your needs seamlessly. Your tasks, your way. Start customizing today!'>
+          </HomeItems>
+          </>
+       };           
 
 export default Home;
